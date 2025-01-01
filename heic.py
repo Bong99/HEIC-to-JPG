a4747d51f32778d5e972048e0f4537e3f97e8979
Bong99 <bong99@gmail.com>
2025-01-01 20:52:35 +0800

    First Release
    There is a windows execute file (*.exe) in the output folder.

---
+++ heic.py
@@ -0,0 +1,121 @@
+﻿
+import os
+import sys
+from PIL import Image
+from pillow_heif import register_heif_opener
+import tkinter as tk
+from tkinter import ttk
+import tkinterDnD  # Importing the tkinterDnD module
+
+
+register_heif_opener()
+a = 0
+
+# Convert the heic file into jpg
+def convert_to_jpg(input_file, output_file):
+    try:
+        img = Image.open(input_file)
+        img = img.convert("RGB")
+        img.save(output_file, "JPEG", quality=95)
+        #print(f"Successfully converted {input_file} to {output_file}")
+    except Exception as e:
+        print(f"Error converting {input_file} to {output_file}: {e}")
+
+# Remove all items in the listbox and reset the progress bar
+def clearList():
+    listbox.delete(0, listbox.size())
+    a = 0
+    bar['value'] = a
+
+# Handle the files in listbox 1 by 1, and update the progress bar
+def convertnow():
+    global a # precentage value for progress bar
+    current_index = 1
+    get_content = listbox.get(0, listbox.size()) # get a tuple from listbox
+
+    # Handle the items in listnox 1 by 1
+    for input_file in get_content:
+        #print(input_file)
+        output_file = os.path.splitext(input_file)[0] + ".jpg"
+        
+        a = current_index/listbox.size() * 100
+        
+        bar['value'] = a
+        root.update()
+        current_index = current_index+1
+        
+        convert_to_jpg(input_file, output_file)
+
+# You have to use the tkinterDnD.Tk object for super easy initialization,
+# and to be able to use the main window as a dnd widget
+root = tkinterDnD.Tk()
+root.title("HEIC to JPG")
+
+stringvar = tk.StringVar()
+stringvar.set('Drag file (HEIC file only, not support folder) to here to convert +')
+
+
+def drop(event):
+    # This function is called, when stuff is dropped into a widget
+#    stringvar.set(event.data)
+    #print("DROP!")
+    #print(event.data)
+    
+    if event.data[0] == '{':
+        #print('has Space')
+        x = event.data.split("} {")
+        for item in x:
+            item = item.replace('{', '')
+            item = item.replace('}', '')
+            file_ext = os.path.splitext(item.lower())[1]
+            if file_ext == '.heic':
+                listbox.insert(0, item)
+    else:
+        #print("No Space")
+        x = event.data.split()
+        for item in x:
+            file_ext = os.path.splitext(item.lower())[1]
+            if file_ext == '.heic':
+                listbox.insert(0, item)
+
+
+def drag_command(event):
+    # This function is called at the start of the drag,
+    # it returns the drag type, the content type, and the actual content
+    print("DRAG!")
+    return (tkinterDnD.COPY, "DND_Text", "Some nice dropped text!")
+
+
+# UI Layout section start
+
+# With DnD hook you just pass the command to the proper argument,
+# and tkinterDnD will take care of the rest
+# NOTE: You need a ttk widget to use these arguments
+
+
+about_label_text = ('Author: Paul Chow\n'
+    'Email: bong99@gmail.com\n'
+    'License: MIT License\n\n'
+    'Drag the HEIC files to below area, then press "Convert" button to start convert.\n'
+    'The converted file will be saved in the same folder'
+    )
+
+label_about = tk.Label(root, text=about_label_text)
+label_about.pack()   
+
+label_2 = ttk.Label(root, ondrop=drop, ondragstart=drag_command, textvar=stringvar, padding=50, relief="solid")
+label_2.pack(fill="both", expand=True, padx=10, pady=10)
+
+listbox = tk.Listbox(root)
+listbox.pack(fill="both", expand=True, padx=10, pady=10)
+
+button1 = tk.Button(text = "Convert", command = convertnow)
+button1.pack(padx = 3, pady = 3)
+
+button2 = tk.Button(text = "Clear", command = clearList)
+button2.pack(padx = 3, pady = 3)
+
+bar = ttk.Progressbar(root)
+bar.pack(pady=20)
+
+root.mainloop()
